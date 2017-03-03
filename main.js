0d655633fe93486cbb5715e2c7a2ff67873e03b9
Sineos <monk@gmx.net>
2017-03-03 09:49:30 +0100

    3rd take on mem leak

--- main.js
+++ main.js
@@ -24,6 +24,55 @@ var adapter       = utils.adapter({
 
 var memwatch = require('memwatch-ng');
 var heapDiff = null;
+heapDiff = new memwatch.HeapDiff();
+
+var heapdump = require('heapdump');
+
+
+memwatch.on('leak', function(info) {  
+	var myoutput = JSON.stringify(info);
+	adapter.log.error('Leak detected: ' + myoutput);
+	heapdump.writeSnapshot('/opt/iobroker/log/' + Date.now() + '.heapsnapshot');
+});
+
+var filename = '/opt/iobroker/log/memstats'  
+var firstLine = true;
+
+memwatch.on("stats", function(stats) {
+	if (heapDiff) {
+		var diff = heapDiff.end();
+		adapter.log.error('Diff: ' + JSON.stringify(diff));
+	}
+	heapDiff = new memwatch.HeapDiff();
+    
+	var fs = require("fs"),
+        info = [];
+
+    if(firstLine) {
+        info.push("num_full_gc");
+        info.push("num_inc_gc");
+        info.push("heap_compactions");
+        info.push("usage_trend");
+        info.push("estimated_base");
+        info.push("current_base");
+        info.push("min");
+        info.push("max");
+        fs.appendFileSync(filename, info.join(",") + "\n");
+        info = [];
+        firstLine = false;
+    }
+
+    info.push(stats["num_full_gc"]);
+    info.push(stats["num_inc_gc"]);
+    info.push(stats["heap_compactions"]);
+    info.push(stats["usage_trend"]);
+    info.push(stats["estimated_base"]);
+    info.push(stats["current_base"]);
+    info.push(stats["min"]);
+    info.push(stats["max"]);
+
+    fs.appendFile(filename, info.join(",") + "\n");
+});
 
 
 process.on('SIGINT', stop);
